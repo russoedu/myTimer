9d9f646863b6d9a240ccecd1511601c2916499aa
Eduardo Russo <russoedu@gmail.com>
2017-07-27 07:07:55 -0300

    [master] eslint config

--- index.js
+++ index.js
@@ -7,32 +7,27 @@ const Time = require('./lib/Time');
 
 // Start the status and the start time
 Status.logTime();
-const time = new Time()
+const time = new Time();
 debug(time.getStart());
 
 const timersFolder = './timers/';
-const globalTimerPath = path.join(timersFolder, 'default.json');
-
-// Read the timers global configuration
-const globalTimer = JSON.parse(fs.readFileSync(globalTimerPath), 'utf8');
 
 // Set a global var to stop execution after all timers finish execution
 // TODO fix this
 // global.runningTimer = files.length;
 
 const files = fs.readdirSync(timersFolder);
- {};
 
-files.forEach(file => {
+files.forEach((file) => {
   const filePath = path.join(timersFolder, file);
 
   // Read each file content
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-    } else if (file !== 'default.json'){
+    } else if (file !== 'default.json') {
       const timer = new Timer(data, file);
       timer.start();
     }
   });
-})
+});
